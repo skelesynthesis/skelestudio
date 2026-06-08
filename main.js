6e6fc55c3a0c62494b2d41e2ef2c2a3dd447098a
skelesynthesis <skelesynthesis@gmail.com>
2026-06-08 17:38:42 -0500

    Update main.js
    fuck it

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 var gameData = {
-
-//store
-    seedsCost: 5,
-
-
-//inventory 
-
-    seedsOwned: 0,
-    moneyOwned: 20,
+  gold: 0,
+  goldPerClick: 1,
+  goldPerClickCost: 10
 }
 
-function buySeeds() {
-        gameData.seedsOwned += 1
-        document.getElementByID("seedsOwned").innerHTML = gameData.seedsOwned + " seeds owned"
+function mineGold() {
+  gameData.gold += gameData.goldPerClick
+  document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
 }
+
+function buyGoldPerClick() {
+  if (gameData.gold >= gameData.goldPerClickCost) {
+    gameData.gold -= gameData.goldPerClickCost
+    gameData.goldPerClick += 1
+    gameData.goldPerClickCost *= 2
+    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
+  }
+}
+
+var mainGameLoop = window.setInterval(function() {
+  mineGold()
+}, 1000)
